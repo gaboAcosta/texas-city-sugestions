d8f33b0e32344cc07d687346cfa78448e55becd5
Gabriel Acosta <gabriel.acosta@gdla-mac-150112a.local>
2016-09-08 14:11:10 -0500

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    express: {
+      options: {
+        // Override defaults here
+      },
+      dev: {
+        options: {
+          script: 'server/app.js'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  grunt.registerTask('server', [ 'express:dev', 'watch' ])
+  grunt.registerTask('default', ['server']);
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+Easy peasy:
+
+Install Grunt if not installed yet
+
+`npm install -g grunt-cli`
+
+Install dependencies
+
+`npm install`
+
+Run default grunt task
+
+`grunt`
